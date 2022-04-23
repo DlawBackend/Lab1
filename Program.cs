6a8843d0a5c72672ad0e5113660f1d0bbf6b3497
wellahealthschool github <dlaweyenghe@gmil.com>
2022-04-23 02:00:22 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int i, num1 = 0, flage = 0;  
+            Console.Write("Enter a number: ");
+            String input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            num1 = number/2;
+            for(i = 2; i <= num1; i++) {
+                if (number % i == 0) {
+                    Console.WriteLine("Number is not prime");
+                    flage = 1;
+                    break;
+                }
+            }
+
+            if (flage == 0)
+                Console.WriteLine("number is prime.");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
